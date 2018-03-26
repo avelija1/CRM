20c8fcb011ad405de5c467fa254d5692c5880c84
infxmous <mirhad_nl@hotmail.com>
2018-03-26 13:09:58 +0200

    Tabcontrol

--- CRMVersion1.0/CRMVersion1.0/MainNavigation.xaml.cs
+++ CRMVersion1.0/CRMVersion1.0/MainNavigation.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +25,25 @@ namespace CRMVersion1._0
         private static bool addMode = true;
         private static int idOfCustomerToEdit = -1;//auxiliary variable
         private CRMV1Entities1 _context;
+        private string fileName;
+        private byte[] imageInBytes;
+  
 
         public MainNavigation()
         {
             InitializeComponent();
             _context = new CRMV1Entities1();//Open connection on windows initialization and dispose on window_unloaded even
+            if (_context.Companies.Count() != 0)
+            {
+                Company myCompany = _context.Companies.First();
+                CompanyNameTB.Text = myCompany.Name;
+                AdressTB.Text = myCompany.Adress;
+                TelephoneTB.Text = myCompany.Telefon;
+                WebSiteTB.Text = myCompany.Website;
+                image.Source = new BitmapImage(new Uri(myCompany.FileName));
+                fileName = myCompany.FileName;
+                imageInBytes = myCompany.Data;
+            }
 
         }
 
@@ -238,6 +254,74 @@ namespace CRMVersion1._0
 
 
 
+        //------------------------------------------------------------------Customer Screen--------------------------------------------------------
 
+   
+
+        private void btnLogo_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPEG|*.jpg", ValidateNames = true, Multiselect = false };
+            if (ofd.ShowDialog() == true)
+            {
+                fileName = ofd.FileName;
+                image.Source = new BitmapImage(new Uri(fileName));
+            }
+            imageInBytes = getJPGFromImageControl(image.Source as BitmapImage);
+        }
+
+        private byte[] getJPGFromImageControl(BitmapImage bitmapImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnSaveComp_Click(object sender, RoutedEventArgs e)
+        {
+            //Add Company
+            if (CheckIfDataInFieldsAreValid())//Validation on form needs to be implemented
+            {
+                if (_context.Companies.Count() == 0)
+                {
+                    try
+                    {
+                        Company newCompany = new Company
+                        {
+                            Name = CompanyNameTB.Text,
+                            Adress = AdressTB.Text,
+                            Telefon = TelephoneTB.Text,
+                            Website = WebSiteTB.Text,
+                            FileName = fileName,
+                            Data = imageInBytes
+                        };
+                        _context.Companies.Add(newCompany);
+                        _context.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Company add error: " + ex.Message);
+                    }
+                }//Edit Company
+                else
+                {
+                    try
+                    {
+                        Company existingCompany = _context.Companies.FirstOrDefault();
+                        if (existingCompany != null)
+                        {
+                            existingCompany.Name = CompanyNameTB.Text;
+                            existingCompany.Adress = AdressTB.Text;
+                            existingCompany.Telefon = TelephoneTB.Text;
+                            existingCompany.Website = WebSiteTB.Text;
+                            existingCompany.FileName = fileName;
+                            existingCompany.Data = imageInBytes;
+                        }
+                        _context.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Company edit error: " + ex.Message);
+                    }
+                }
+            }
+        }
     }
 }
