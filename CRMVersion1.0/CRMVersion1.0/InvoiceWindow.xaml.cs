9f2cd6b74fa77ac3827bd6505a9db0f419befe72
Almedin Velija <almedin.velija@gmail.com>
2018-03-31 16:43:50 +0200

    Validation implemented on  Invoice Window, Add/Edit/Remove items in datagrid implemented

--- CRMVersion1.0/CRMVersion1.0/InvoiceWindow.xaml.cs
+++ CRMVersion1.0/CRMVersion1.0/InvoiceWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Windows.Controls.Primitives;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Text;
@@ -22,14 +23,38 @@ namespace CRMVersion1._0
     public partial class InvoiceWindow : Window
     {
         public List<Service> Services { get; set; }
-        public
-            DataTable dt
-        { get; set; }
+        public int quantity;
+        public static int idOfDatagridItemToEdit = -1;
+        public decimal Total { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DataTable dt { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public InvoiceWindow()
         {
             InitializeComponent();
-            CRMV1Entities _contex = new CRMV1Entities();
-            Services = _contex.Services.ToList();
+            SwitchMode();
+            using (CRMV1Entities _context = new CRMV1Entities())
+            {
+                Services = new List<Service>();
+                Services.Add(new Service());
+                InvoiceDate = DateTime.Now;
+                foreach (var service in _context.Services)
+                {
+                    Services.Add(service);
+                }
+                long id;
+                if (_context.Services.Count() > 0)
+                {
+                    List<Invoice> c = _context.Invoices.ToList();
+                    id = c[_context.Invoices.Count() - 1].Id + 1;
+                }
+                else
+                {
+                    id = 1;
+                }
+                InvoiceNoTB.Text = id.ToString();
+            }
             DataContext = this;
 
             dt = new DataTable();
@@ -42,104 +67,224 @@ namespace CRMVersion1._0
             dataGrid.ItemsSource = dt.DefaultView;
         }
 
+        public int Quantity
+        {
+            get { return quantity; }
+            set
+            {
+                quantity = value;
+                if (PriceTB.Text != "")
+                {
+                    TotalTB.Text = (Convert.ToDecimal(PriceTB.Text) * quantity).ToString();
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
         private void OnMyComboBoxChanged(object sender, SelectionChangedEventArgs e)
         {
+           
             var comboBox = sender as ComboBox;
-            Service s = (Service)comboBox.SelectedItem;
-            PriceTB.Text = s.Price.ToString();
-            QuantityTB.Text = "1";
-            TotalTB.Text = PriceTB.Text;
+            if (comboBox.SelectedIndex != -1 && comboBox.SelectedIndex!=0)
+            {
+                Service s = (Service)comboBox.SelectedItem;
+                PriceTB.Text = s.Price.ToString();
+                QuantityTB.Text = "1";
+                TotalTB.Text = PriceTB.Text;
+            }
+            else
+            {
+                PriceTB.Text = "";
+                QuantityTB.Text = "0";
+                TotalTB.Text = "";
+            }
         }
 
-        //Add button
+        //Add item button
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            ServiceItem si = new ServiceItem();
-            si.Id = Convert.ToInt32(((Service)ServiceNameCB.SelectedItem).Id);
-            si.LineNumber = dataGrid.Items.Count;
-            si.ServiceName = ServiceNameCB.Text;
-            si.Price = Convert.ToDecimal(PriceTB.Text);
-            si.Quantity = Convert.ToInt32(QuantityTB.Text);
-            si.Total = TotalTB.Text;
-            dt.Rows.Add(si.LineNumber,si.Id,si.ServiceName,si.Price,si.Quantity,si.Total);
-            dataGrid.ItemsSource = dt.DefaultView;
+            if (FieldAreValid())
+            {
+                ServiceItem si = new ServiceItem();
+                si.Id = Convert.ToInt32(((Service)ServiceNameCB.SelectedItem).Id);
+                si.LineNumber = dataGrid.Items.Count;
+                si.ServiceName = ServiceNameCB.Text;
+                si.Price = Convert.ToDecimal(PriceTB.Text);
+                si.Quantity = Convert.ToInt32(QuantityTB.Text);
+                si.Total = TotalTB.Text;
+                dt.Rows.Add(si.LineNumber, si.Id, si.ServiceName, si.Price, si.Quantity, si.Total);
+                dataGrid.ItemsSource = dt.DefaultView;
+                ClearFields();
+            }
         }
 
+        private bool FieldAreValid()
+        {
+            if(ServiceNameCB.SelectedIndex==0 || ServiceNameCB.SelectedIndex==-1)
+            {
+                ServiceNameCB.SelectedIndex = -1;
+                return false;
+            }
+            if(QuantityTB.Text=="")
+            {
+                QuantityTB.Text = "";
+                return false;
+            }
+
+            return true;
+        }
 
         private void QuantityTB_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (QuantityTB.Text != "")
+            //If it is not number, then trigger validation in catch block!
+           try
             {
-                TotalTB.Text = (Convert.ToDecimal(PriceTB.Text) * Convert.ToInt32(QuantityTB.Text)).ToString();
+                Convert.ToInt32(QuantityTB.Text);
+            }
+            catch
+            {
+                TotalTB.Text = "";
             }
         }
-        //Save button
+        
+        //Note: it is not fully implemented, I will implement it soon!
+        //Save  button - saves invoice
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-          DataTable dt = ((DataView)dataGrid.ItemsSource).ToTable();
-            using (CRMV1Entities _context = new CRMV1Entities())
-            {
-                long id;
-                if (_context.Items.Count() > 0)
-                {
-                    List<Item> c = _context.Items.ToList();
-                    id = c[_context.Items.Count() - 1].Id + 1;
-                }
-                else
-                {
-                    id = 1;
-                }
-                foreach (DataRow row in dt.Rows)
-                {
-                    int serviceId = Convert.ToInt32(row["Id"].ToString());
-                    int quantity = Convert.ToInt32(row["Quantity"].ToString());
-                    decimal total = Convert.ToDecimal(row["Total"].ToString());
-                    Item i = new Item() { Id=id,ServiceID = serviceId, InvoiceID = 2, Quantity = quantity, Total = total };
-                    _context.Items.Add(i);
-                    id++;
-                }
-                _context.SaveChanges();
-            }
+            // DataTable dt = ((DataView)dataGrid.ItemsSource).ToTable();
+            //using (CRMV1Entities _context = new CRMV1Entities())
+            //{
+            //    long id;
+            //    if (_context.Items.Count() > 0)
+            //    {
+            //        List<Item> c = _context.Items.ToList();
+            //        id = c[_context.Items.Count() - 1].Id + 1;
+            //    }
+            //    else
+            //    {
+            //        id = 1;
+            //    }
+            //    foreach (DataRow row in dt.Rows)
+            //    {
+            //        int serviceId = Convert.ToInt32(row["Id"].ToString());
+            //        int quantity = Convert.ToInt32(row["Quantity"].ToString());
+            //        decimal total = Convert.ToDecimal(row["Total"].ToString());
+            //        Item i = new Item() { Id=id,ServiceID = serviceId, InvoiceID = 2, Quantity = quantity, Total = total };
+            //        _context.Items.Add(i);
+            //        id++;
+            //    }
+            //    _context.SaveChanges();
+            //}
         }
 
-        public static T GetVisualChild<T>(Visual parent) where T : Visual
+
+
+        private void EditButtonEvent(object sender, RoutedEventArgs e)
         {
-            T child = default(T);
-            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
-            for (int i = 0; i < numVisuals; i++)
+            try
             {
-                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
-                child = v as T;
-                if (child == null)
+                SwitchMode(false);
+                var button = (Button)sender;
+                int id = Convert.ToInt32(button.CommandParameter);
+                if (id != 0)//Case when we do not have any data in row
                 {
-                    child = GetVisualChild<T>(v);
+                    idOfDatagridItemToEdit = id;
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        int serviceId = Convert.ToInt32(row["Id"].ToString());
+                        if (serviceId == id)
+                        {
+                            ServiceNameCB.SelectedValue = id;
+                            QuantityTB.Text = row["Quantity"].ToString();
+                            TotalTB.Text = row["Total"].ToString();
+                            PriceTB.Text = row["Price"].ToString();
+                        }
+                    }
                 }
-                if (child != null)
-                {
-                    break;
-                }
+
             }
-            return child;
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in delete button event: " + ex.Message);
+            }
         }
 
-        public static DataGridCell GetCell(DataGrid grid, int column, int rowN)
+        private void ClearFields()
         {
-            DataGridRow row = grid.ItemContainerGenerator.ContainerFromIndex(rowN) as DataGridRow;
-
-            if (row != null)
-            {
-                DataGridCellsPresenter presenter = GetVisualChild<DataGridCellsPresenter>(row);
-
-                if (presenter == null)
-                {
-                    grid.ScrollIntoView(row, grid.Columns[column]);
-                    presenter = GetVisualChild<DataGridCellsPresenter>(row);
-                }
-
-                DataGridCell cell = (DataGridCell)presenter.ItemContainerGenerator.ContainerFromIndex(column);
-                return cell;
-            }
-            return null;
+            ServiceNameCB.SelectedIndex = 0;
+            PriceTB.Text = "";
+            QuantityTB.Text = "0";
+            TotalTB.Text = "";
         }
+
+        private void DeleteButtonEvent(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var button = (Button)sender;
+                int id = Convert.ToInt32(button.CommandParameter);
+
+                if (id != 0)//Case when we do not have any data in row
+                {
+                    int indexToRemove = -1;
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        int serviceId = Convert.ToInt32(row["Id"].ToString());
+                        if (serviceId == id)
+                        {
+                            indexToRemove = Convert.ToInt32(row["LineNumber"].ToString());
+                        }
+                    }
+                    dt.Rows.RemoveAt(indexToRemove - 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in delete button event: " + ex.Message);
+            }
+        }
+
+        private void SaveItemChangesButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (DataRow row in dt.Rows)
+            {
+                int serviceId = Convert.ToInt32(row["Id"].ToString());
+                if (serviceId == idOfDatagridItemToEdit)
+                {
+                    row["ServiceName"] = ServiceNameCB.Text.ToString();
+                    row["Quantity"]=QuantityTB.Text;
+                    row["Total"]=TotalTB.Text;
+                    row["Price"]=PriceTB.Text;
+                }
+            }
+            ClearFields();
+            SwitchMode();
+        }
+        public void SwitchMode(bool addItemMode = true)
+        {
+            if (addItemMode)
+            {
+                AddItemButton.Visibility = System.Windows.Visibility.Visible;
+                CancelEditButton.Visibility = System.Windows.Visibility.Hidden;
+                SaveItemChangesButton.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else
+            {
+                AddItemButton.Visibility = System.Windows.Visibility.Hidden;
+                CancelEditButton.Visibility = System.Windows.Visibility.Visible;
+                SaveItemChangesButton.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        private void CancelEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearFields();
+            SwitchMode();
+        }
+
     }
 }
